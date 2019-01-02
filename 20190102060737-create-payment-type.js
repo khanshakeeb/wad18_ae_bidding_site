9496f9300459e8a1d6ae1ae6f45ee8a4f85b0e37
tabishikram <44540238+tabishikram@users.noreply.github.com>
2019-01-02 12:19:49 +0500

    Add files via upload

---
+++ 20190102060737-create-payment-type.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Payment_Types', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      Id: {
+        type: Sequelize.INTEGER
+      },
+      Name: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Payment_Types');
+  }
+};
